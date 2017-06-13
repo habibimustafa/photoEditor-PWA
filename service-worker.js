9030698d086c25b475d72981db79f3adf756e0a5
Habibi Mustafa <bibitahabibi@gmail.com>
2017-06-14 06:53:19 +0700

    Fix sw cache path

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var cacheName     = 'photoEditorPWA-v0.1';
 var filesToCache  = [
-  '/',
-  '/index.html',
-  '/scripts/app.js',
-  '/styles/inline.css',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/heroteesme.jpg'
+  './',
+  './index.html',
+  './scripts/app.js',
+  './styles/inline.css',
+  './images/ic_add_white_24px.svg',
+  './images/ic_refresh_white_24px.svg',
+  './images/heroteesme.jpg'
 ];
 
 self.addEventListener('install', function(e) {
